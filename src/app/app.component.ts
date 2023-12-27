499832aa0d78a49f23461af1956291f0a5be5cbe
evelyne802 <linagluzman@gmail.com>
2023-12-27 20:52:57 +0200

    fix: first row keys size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'myApp';
+  title = 'VirtualKeyboard';
+  
+  firstRow = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
+  firstRowClasses = [''];
+
+  secondRow = [];
+  secondRowClasses = [];
+
+  thirdRow = [];
+  thirdRowClasses = [];
+
+  fourthRow = [];
+  fourthRowClasses = [];
+
+  fifthRow = [];
+  fifthRowClasses = [];
+
+  ngOnInit(){
+    
+  }
 }
